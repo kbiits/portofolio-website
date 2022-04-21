eab8bf0b699430f518a543774b3c6e0a9dbc9df6
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2022-04-21 13:24:34 +0700

    init

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,96 +2,66 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'kbiits', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 8, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
-      projects: ['laravel-ecommerce'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      forks: true, // Forked projects will not be displayed if set to true.
+      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
+    linkedin: 'nabiel',
+    twitter: '',
     facebook: '',
     dribbble: '',
     behance: '',
-    medium: '',
-    dev: 'arifszn',
-    website: 'https://arifszn.github.io',
+    medium: 'kbiits',
+    dev: '',
+    website: 'https://nabiels.my.id',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'nabilumarsyarif294@gmail.com',
   },
   skills: [
-    'PHP',
+    'Golang',
+    'Docker',
     'Laravel',
-    'JavaScript',
+    'Linux',
     'React.js',
     'Node.js',
-    'MySQL',
-    'Git',
-    'Docker',
-    'CSS',
-    'Antd',
-    'Tailwind',
-    'Bootstrap',
   ],
   experiences: [
     {
-      company: 'Monstarlab Bangladesh',
-      position: 'Backend Engineer II',
-      from: 'September 2021',
-      to: 'Present',
-    },
-    {
-      company: 'Orangetoolz',
-      position: 'Jr. Full Stack Engineer',
-      from: 'July 2019',
-      to: 'August 2021',
-    },
-    {
-      company: 'Techvillage',
-      position: 'Jr. Software Engineer',
-      from: 'January 2019',
-      to: ' June 2019',
+      company: 'Waresix',
+      position: 'Frontend Engineer Intern',
+      from: 'Dec 2021',
+      to: 'Mar 2022',
     },
   ],
   education: [
     {
-      institution: 'American International University-Bangladesh',
-      degree: 'Bachelor of Science',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Cantonment College, Jessore',
-      degree: 'Higher Secondary Certificate (HSC)',
-      from: '2012',
-      to: '2014',
-    },
-    {
-      institution: 'Chowgacha Shahadat Pilot High School',
-      degree: 'Secondary School Certificate (SSC)',
-      from: '2007',
-      to: '2012',
+      institution: 'Sriwijaya University',
+      degree: 'Computer Science',
+      from: '2020',
+      to: 'present',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn',
-    limit: 3, // How many posts to display. Max is 10.
+    source: 'medium', // medium | dev
+    username: 'kbiits',
+    limit: 5, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-    id: 'G-WLLB5E14M6', // Please remove this and use your own tag id or keep it empty
+    id: '', // Please remove this and use your own tag id or keep it empty
   },
   hotjar: {
-    id: '2617601', //  Please remove this and use your own id or keep it empty
+    id: '', //  Please remove this and use your own id or keep it empty
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'corporate',
+    defaultTheme: 'halloween',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
--- src/components/head-tag-editor/index.jsx
+++ src/components/head-tag-editor/index.jsx
@@ -22,7 +22,7 @@ const HeadTagEditor = ({ profile, theme, googleAnalytics, social }) => {
                 gtag('config', '${googleAnalytics.id}');`}
             </script>
           )}
-          <title>Portfolio of {profile.name}</title>
+          <title>{profile.name}</title>
           <meta
             name="theme-color"
             content={isThemeDarkish(theme) ? '#000000' : '#ffffff'}
